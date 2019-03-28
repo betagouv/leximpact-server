f43cc0347c05b5df305d911a5a18cb880bda5eae
Dorine Lambinet <dorinelambinet@MacBook-Pro-de-Dorine.local>
2019-03-28 18:20:35 +0100

    Première modification de la charte CSS
    Ajout de l'image en background, changement des typographies, Nameclasse des div pour les séparer et les transformer séparemment, déplacement et transformation du bouton "submit"

--- InterfaceReform/OutputImpactCasType.py
+++ InterfaceReform/OutputImpactCasType.py
@@ -23,53 +23,83 @@ basevalue=9964
 sizeperc=1
 sizev=3
 
-app.layout = html.Div([html.Link(href="https://fonts.googleapis.com/css?family=Cormorant+Garamond",rel="stylesheet"),
-    html.Div([html.H1("Article 197"),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.P("""I. – En ce qui concerne les contribuables visés à l'article 4 B, il est fait application des règles suivantes pour le calcul de l'impôt sur le revenu :"""),
-    html.P(html.Div(["""1. L'impôt est calculé en appliquant à la fraction de chaque part de revenu qui excède """,
-                     dcc.Input(id='input-seuil0', type='text', value=9964,size=sizev),"""€ le taux de :"""])),
-    html.P(html.Div(["""– """, dcc.Input(id='input-taux0', type='text', value=14,size=sizeperc),"""% pour la fraction supérieure à """,
-                     html.B(id='output-seuil0'),
-                     """ et inférieure ou égale à""", dcc.Input(id='input-seuil1', type='text', value=27519,size=sizev),"""€ ;"""])),
-    html.P(html.Div(["""– """, dcc.Input(id='input-taux1', type='text', value=30,size=sizeperc),"""% pour la fraction supérieure à """,
-                     html.B(id='output-seuil1'),
-                     """ € et inférieure ou égale à""", dcc.Input(id='input-seuil2', type='text', value=73779,size=sizev),"""€ ;"""])),
-    html.P(html.Div(["""– """, dcc.Input(id='input-taux2', type='text', value=41,size=sizeperc),"""% pour la fraction supérieure à """,
-                     html.B(id='output-seuil2'),
-                     """ € et inférieure ou égale à""", dcc.Input(id='input-seuil3', type='text', value=156244,size=sizev),"""€ ;"""])),
-    html.P(html.Div(["""– """, dcc.Input(id='input-taux3', type='text', value=45,size=sizeperc),"""% pour la fraction supérieure à""",
-                     html.B(id='output-seuil3'),
-                     """€"""])),
-    html.Div(["""2. La réduction d'impôt résultant de l'application du quotient familial ne peut excéder""",
-              """ 1 527 € par demi-part ou la moitié de cette somme par quart de part s'ajoutant à une part pour les contribuables célibataires, divorcés, veufs ou soumis à l'imposition distincte prévue au 4 de l'article 6 et à deux parts pour les contribuables mariés soumis à une imposition commune."""]),
-    html.P("""Toutefois, pour les contribuables célibataires, divorcés, ou soumis à l'imposition distincte prévue au 4 de l'article 6 qui répondent aux conditions fixées au II de l'article 194, la réduction d'impôt correspondant à la part accordée au titre du premier enfant à charge est limitée à 3 660 € Lorsque les contribuables entretiennent uniquement des enfants dont la charge est réputée également partagée entre l'un et l'autre des parents, la réduction d'impôt correspondant à la demi-part accordée au titre de chacun des deux premiers enfants est limitée à la moitié de cette somme."""),
-    html.P("""Par dérogation aux dispositions du premier alinéa, la réduction d'impôt résultant de l'application du quotient familial, accordée aux contribuables qui bénéficient des dispositions des a, b et e du 1 de l'article 195, ne peut excéder 927 € ;"""),
-    html.P("""Les contribuables qui bénéficient d'une demi-part au titre des a, b, c, d, d bis, e et f du 1 ainsi que des 2 à 6 de l'article 195 ont droit à une réduction d'impôt égale à 1 547 € pour chacune de ces demi-parts lorsque la réduction de leur cotisation d'impôt est plafonnée en application du premier alinéa. La réduction d'impôt est égale à la moitié de cette somme lorsque la majoration visée au 2 de l'article 195 est de un quart de part. Cette réduction d'impôt ne peut toutefois excéder l'augmentation de la cotisation d'impôt résultant du plafonnement."""),
-    html.P("""Les contribuables veufs ayant des enfants à charge qui bénéficient d'une part supplémentaire de quotient familial en application du I de l'article 194 ont droit à une réduction d'impôt égale à 1 728 € pour cette part supplémentaire lorsque la réduction de leur cotisation d'impôt est plafonnée en application du premier alinéa du présent 2. Cette réduction d'impôt ne peut toutefois excéder l'augmentation de la cotisation d'impôt résultant du plafonnement."""),
-    html.P("""3. Le montant de l'impôt résultant de l'application des dispositions précédentes est réduit de 30 %, dans la limite de 2 450 €, pour les contribuables domiciliés dans les départements de la Guadeloupe, de la Martinique et de la Réunion ; cette réduction est égale à 40 %, dans la limite de 4 050 €, pour les contribuables domiciliés dans les départements de la Guyane et de Mayotte ;"""),
-    html.P("""4. a. Le montant de l'impôt résultant de l'application des dispositions précédentes est diminué, dans la limite de son montant, de la différence entre 1 196 € et les trois quarts de son montant pour les contribuables célibataires, divorcés ou veufs et de la différence entre 1 970 € et les trois quarts de son montant pour les contribuables soumis à imposition commune."""),
-    html.P("""b. Le montant de l'impôt résultant du a est réduit dans les conditions prévues au sixième alinéa du présent b pour les contribuables dont le montant des revenus du foyer fiscal, au sens du 1° du IV de l'article 1417, est inférieur à 20 500 €, pour la première part de quotient familial des personnes célibataires, veuves ou divorcées, et à 41 000 €, pour les deux premières parts de quotient familial des personnes soumises à une imposition commune. Ces seuils sont majorés de 3 700 € pour chacune des demi-parts suivantes et de la moitié de ce montant pour chacun des quarts de part suivants."""),
-    html.P("""Pour l'application des seuils mentionnés au premier alinéa du présent b, le montant des revenus du foyer fiscal est majoré :"""),
-    html.P("""1° Du montant des plus-values, déterminées le cas échéant avant application des abattements pour durée de détention mentionnés au 1 de 150-0 D bis, dans leur rédaction en vigueur jusqu'au 31 décembre 2013 ;"""),
-    html.P("""2° Du montant des plus-values, déterminées le cas échéant avant application des abattements pour durée de détention mentionnés aux 1 ter ou 1 quater de l'article 150-0 D ou à l'article 150-0 D ter, et des créances mentionnées aux I et II de l'article 167 bis, pour la seule détermination du premier terme de la différence mentionnée au deuxième alinéa du 1 du II bis du même article 167 bis ;"""),
-    html.P("""3° Du montant des plus-values mentionnées au I de 200 A pour l'application de la seconde phrase du 3° du même a."""),
-    html.P("""Le taux de la réduction prévue au premier alinéa du présent b est de 20 %. Toutefois, pour les contribuables dont les revenus du foyer fiscal, au sens du 1° du IV de l'article 1417, excèdent 18 500 €, pour la première part de quotient familial des personnes célibataires, veuves ou divorcées, ou 37 000 €, pour les deux premières parts de quotient familial des personnes soumises à une imposition commune, ces seuils étant majorés le cas échéant dans les conditions prévues au même premier alinéa, le taux de la réduction d'impôt est égal à 20 % multiplié par le rapport entre :"""),
-    html.P("""– au numérateur, la différence entre 20 500 €, pour les personnes célibataires, veuves ou divorcées, ou 41 000 €, pour les personnes soumises à une imposition commune, ces seuils étant majorés le cas échéant dans les conditions prévues audit premier alinéa, et le montant des revenus mentionnés au troisième alinéa du présent b, et ;"""),
-    html.P("""– au dénominateur, 2 000 €, pour les personnes célibataires, veuves ou divorcées, ou 4 000 €, pour les personnes soumises à une imposition commune."""),
-    html.P("""Les montants de revenus mentionnés au présent b sont révisés chaque année dans la même proportion que la limite supérieure de la première tranche du barème de l'impôt sur le revenu. Les montants obtenus sont arrondis, s'il y a lieu, à l'euro supérieur."""),
-    html.P("""5. Les réductions d'impôt mentionnées aux articles 199 quater B à 200 s'imputent sur l'impôt résultant de l'application des dispositions précédentes avant imputation des crédits d'impôt et des prélèvements ou retenues non libératoires ; elles ne peuvent pas donner lieu à remboursement."""),
-   # dcc.Input(id='input-seuil0', type='text', value='1527'),
-    html.B(id='result-reform')
-],style={'width': '49%', 'display': 'inline-block'}),
-    html.Div([html.P(dcc.Graph(
-        id='graphtotal'
+# DORINE - ajout d'un lien vers l'image du fond 
+app.py = html.Img(src=app.get_asset_url('trame-background.png')),
+
+# DORINE - AJOUTER UN HEADER /// ÇA MARCHE PAS.
+app.layout = html.Div([
+    html.Div(
+        className="header",
+        children=[
+            html.Div('Plotly Dash', className="onglet1")
+        ]
+    ),
+    html.Div(
+        children=html.Div([
+            html.H5('Overview'),
+            html.Div('''
+                This is an example of a simple Dash app with
+                local, customized CSS.
+            ''')
+        ])
+    )
+])
+# DORINE AJOUTER UN HEADER 
+
+app.layout = html.Div([html.Link(href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i|PT+Serif",rel="stylesheet"),
+    html.Link(href="https://fonts.googleapis.com/css?family=Lato",rel="stylesheet"),
+    html.Link(href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i|PT+Serif",rel="stylesheet"),
+#DÉBUT DU BLOC ARTICLE   
+    html.Div([html.H2("Article 197"),
+        html.H3("code général des impôts"),#DORINE - AJOUT DU TITRE
+        html.P("""I. – En ce qui concerne les contribuables visés à l'article 4 B, il est fait application des règles suivantes pour le calcul de l'impôt sur le revenu :"""),
+        html.P(html.Div(["""1. L'impôt est calculé en appliquant à la fraction de chaque part de revenu qui excède """,
+                         dcc.Input(id='input-seuil0', type='text', value=9964,size=sizev),"""€ le taux de :"""])),
+        html.P(html.Div(["""– """, dcc.Input(id='input-taux0', type='text', value=14,size=sizeperc),"""% pour la fraction supérieure à """,
+                         html.B(id='output-seuil0'),
+                         """ et inférieure ou égale à""", dcc.Input(id='input-seuil1', type='text', value=27519,size=sizev, className="typovaleur"),"""€ ;"""])),
+        html.P(html.Div(["""– """, dcc.Input(id='input-taux1', type='text', value=30,size=sizeperc),"""% pour la fraction supérieure à """,
+                         html.B(id='output-seuil1'),
+                         """ € et inférieure ou égale à""", dcc.Input(id='input-seuil2', type='text', value=73779,size=sizev),"""€ ;"""])),
+        html.P(html.Div(["""– """, dcc.Input(id='input-taux2', type='text', value=41,size=sizeperc),"""% pour la fraction supérieure à """,
+                         html.B(id='output-seuil2'),
+                         """ € et inférieure ou égale à""", dcc.Input(id='input-seuil3', type='text', value=156244,size=sizev),"""€ ;"""])),
+        html.P(html.Div(["""– """, dcc.Input(id='input-taux3', type='text', value=45,size=sizeperc),"""% pour la fraction supérieure à""",
+                         html.B(id='output-seuil3'),
+                         """€"""])),
+        html.Div(["""2. La réduction d'impôt résultant de l'application du quotient familial ne peut excéder""",
+                  """ 1 527 € par demi-part ou la moitié de cette somme par quart de part s'ajoutant à une part pour les contribuables célibataires, divorcés, veufs ou soumis à l'imposition distincte prévue au 4 de l'article 6 et à deux parts pour les contribuables mariés soumis à une imposition commune."""]),
+        html.P("""Toutefois, pour les contribuables célibataires, divorcés, ou soumis à l'imposition distincte prévue au 4 de l'article 6 qui répondent aux conditions fixées au II de l'article 194, la réduction d'impôt correspondant à la part accordée au titre du premier enfant à charge est limitée à 3 660 € Lorsque les contribuables entretiennent uniquement des enfants dont la charge est réputée également partagée entre l'un et l'autre des parents, la réduction d'impôt correspondant à la demi-part accordée au titre de chacun des deux premiers enfants est limitée à la moitié de cette somme."""),
+        html.P("""Par dérogation aux dispositions du premier alinéa, la réduction d'impôt résultant de l'application du quotient familial, accordée aux contribuables qui bénéficient des dispositions des a, b et e du 1 de l'article 195, ne peut excéder 927 € ;"""),
+        html.P("""Les contribuables qui bénéficient d'une demi-part au titre des a, b, c, d, d bis, e et f du 1 ainsi que des 2 à 6 de l'article 195 ont droit à une réduction d'impôt égale à 1 547 € pour chacune de ces demi-parts lorsque la réduction de leur cotisation d'impôt est plafonnée en application du premier alinéa. La réduction d'impôt est égale à la moitié de cette somme lorsque la majoration visée au 2 de l'article 195 est de un quart de part. Cette réduction d'impôt ne peut toutefois excéder l'augmentation de la cotisation d'impôt résultant du plafonnement."""),
+        html.P("""Les contribuables veufs ayant des enfants à charge qui bénéficient d'une part supplémentaire de quotient familial en application du I de l'article 194 ont droit à une réduction d'impôt égale à 1 728 € pour cette part supplémentaire lorsque la réduction de leur cotisation d'impôt est plafonnée en application du premier alinéa du présent 2. Cette réduction d'impôt ne peut toutefois excéder l'augmentation de la cotisation d'impôt résultant du plafonnement."""),
+        html.P("""3. Le montant de l'impôt résultant de l'application des dispositions précédentes est réduit de 30 %, dans la limite de 2 450 €, pour les contribuables domiciliés dans les départements de la Guadeloupe, de la Martinique et de la Réunion ; cette réduction est égale à 40 %, dans la limite de 4 050 €, pour les contribuables domiciliés dans les départements de la Guyane et de Mayotte ;"""),
+        html.P("""4. a. Le montant de l'impôt résultant de l'application des dispositions précédentes est diminué, dans la limite de son montant, de la différence entre 1 196 € et les trois quarts de son montant pour les contribuables célibataires, divorcés ou veufs et de la différence entre 1 970 € et les trois quarts de son montant pour les contribuables soumis à imposition commune."""),
+        html.P("""b. Le montant de l'impôt résultant du a est réduit dans les conditions prévues au sixième alinéa du présent b pour les contribuables dont le montant des revenus du foyer fiscal, au sens du 1° du IV de l'article 1417, est inférieur à 20 500 €, pour la première part de quotient familial des personnes célibataires, veuves ou divorcées, et à 41 000 €, pour les deux premières parts de quotient familial des personnes soumises à une imposition commune. Ces seuils sont majorés de 3 700 € pour chacune des demi-parts suivantes et de la moitié de ce montant pour chacun des quarts de part suivants."""),
+        html.P("""Pour l'application des seuils mentionnés au premier alinéa du présent b, le montant des revenus du foyer fiscal est majoré :"""),
+        html.P("""1° Du montant des plus-values, déterminées le cas échéant avant application des abattements pour durée de détention mentionnés au 1 de 150-0 D bis, dans leur rédaction en vigueur jusqu'au 31 décembre 2013 ;"""),
+        html.P("""2° Du montant des plus-values, déterminées le cas échéant avant application des abattements pour durée de détention mentionnés aux 1 ter ou 1 quater de l'article 150-0 D ou à l'article 150-0 D ter, et des créances mentionnées aux I et II de l'article 167 bis, pour la seule détermination du premier terme de la différence mentionnée au deuxième alinéa du 1 du II bis du même article 167 bis ;"""),
+        html.P("""3° Du montant des plus-values mentionnées au I de 200 A pour l'application de la seconde phrase du 3° du même a."""),
+        html.P("""Le taux de la réduction prévue au premier alinéa du présent b est de 20 %. Toutefois, pour les contribuables dont les revenus du foyer fiscal, au sens du 1° du IV de l'article 1417, excèdent 18 500 €, pour la première part de quotient familial des personnes célibataires, veuves ou divorcées, ou 37 000 €, pour les deux premières parts de quotient familial des personnes soumises à une imposition commune, ces seuils étant majorés le cas échéant dans les conditions prévues au même premier alinéa, le taux de la réduction d'impôt est égal à 20 % multiplié par le rapport entre :"""),
+        html.P("""– au numérateur, la différence entre 20 500 €, pour les personnes célibataires, veuves ou divorcées, ou 41 000 €, pour les personnes soumises à une imposition commune, ces seuils étant majorés le cas échéant dans les conditions prévues audit premier alinéa, et le montant des revenus mentionnés au troisième alinéa du présent b, et ;"""),
+        html.P("""– au dénominateur, 2 000 €, pour les personnes célibataires, veuves ou divorcées, ou 4 000 €, pour les personnes soumises à une imposition commune."""),
+        html.P("""Les montants de revenus mentionnés au présent b sont révisés chaque année dans la même proportion que la limite supérieure de la première tranche du barème de l'impôt sur le revenu. Les montants obtenus sont arrondis, s'il y a lieu, à l'euro supérieur."""),
+        html.P("""5. Les réductions d'impôt mentionnées aux articles 199 quater B à 200 s'imputent sur l'impôt résultant de l'application des dispositions précédentes avant imputation des crédits d'impôt et des prélèvements ou retenues non libératoires ; elles ne peuvent pas donner lieu à remboursement."""),
+       # dcc.Input(id='input-seuil0', type='text', value='1527'),
+        html.B(id='result-reform')
+],className="inputarticle",style={'width': '53%', 'display': 'inline-block'}),
+#FIN DU BLOC ARTICLE
+
+    html.Div([html.Button(id='submit-button', n_clicks=0, children='Impact sur la population', className="populationbouton"), #DÉPLACEMENT BOUTON COTE STATS
+
+        html.P(dcc.Graph(
+        id='graphtotal',className="outputstat"
     )),
         html.P(dcc.Graph(
-        id='graphdecile'
+        id='graphdecile',className="outputstat"
     ))
 
-    ], style= {'width': '49%', 'display': 'inline-block', 'vertical-align': 'top'})
+    ], style= {'width': '33%', 'display': 'inline-block', 'vertical-align': 'top'})
 ])
 
 
@@ -132,8 +162,14 @@ def get_reform_result(n_clicks,*args):
                     'title': 'changement par décile'
                 }
             }
+
+
     else:
         return None,None
+
+
+
+
 
 #Generates graph
 
