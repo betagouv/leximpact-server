724a9a09fcd5c7a762fdd0368b25d9701f3ad63e
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2019-04-01 16:24:49 +0200

    Supprime visualitation par décile

--- InterfaceReform/OutputImpactCasType.py
+++ InterfaceReform/OutputImpactCasType.py
@@ -79,16 +79,9 @@ app.layout = html.Div([html.Header("Oui oui c'est mon header blabla"),html.Div([
     html.B(id='result-reform')
 ],className="inputarticle six columns"),
     html.Div(
-    graphsCTsplit+
-    # [html.P(dcc.Graph(
-        #      id='graph-ct0'
-    #  )),
-    #      html.P(dcc.Graph(
-        #      id='graph-ct1'
-    #  )),]+
-        [html.P([dcc.Graph(id='graphtotal',className="six columns"),
-            dcc.Graph(id='graphdecile',className="six columns")]
-        ),html.Br(),html.P(desc_cas_types)],className="five columns")],className="row")
+    graphsCTsplit,
+    className="five columns")
+],className="row")
 
 
 # Generates reform text from input. Actually should run the simulations...
@@ -97,13 +90,6 @@ app.layout = html.Div([html.Header("Oui oui c'est mon header blabla"),html.Div([
               [Input(component_id='input-seuil0',component_property=  'value')])
 def output_seuil0(input1):
     return str(input1)
-    # return u"""
-    # def reform_from_bareme(seuilsthreshold=None):
-    #     def TheReform(parameters):
-    #         reform_period = periods.period("year:1900:200")
-    #         parameters.impot_revenu.plafond_qf.maries_ou_pacses.update(period=reform_period, value={})
-    #         return parameters
-    # return TheReform""".format(input1)#.replace("\n","\r\n\r\n"))
 
 @app.callback(Output(component_id='output-seuil1', component_property= 'children'),
               [Input(component_id='input-seuil1',component_property=  'value')])
@@ -120,36 +106,6 @@ def output_seuil2(input1):
 def output_seuil3(input1):
     return str(input1)
 
-# Generates results for the graphs depending on the simulation on the full population
-nbseuil=4
-@app.callback([Output(component_id='graphtotal',component_property= 'figure'),
-               Output(component_id='graphdecile',component_property= 'figure')],
-            [Input(component_id='submit-button', component_property='n_clicks')],
-            [State(component_id='input-seuil{}'.format(numseuil), component_property='value') for numseuil in range(nbseuil)] +
-              [State(component_id='input-taux{}'.format(numseuil), component_property='value') for numseuil in range(nbseuil)])
-def get_reform_result(n_clicks,*args):
-    if n_clicks:
-        myres=simulate_pop_from_reform.CompareOldNew([int(k) for k in args],isdecile=True)#[input1,input1]#
-        return {
-                'data': [
-                    {'x': ["avant"], 'y': [myres["total"]["avant"]], 'type': 'bar', 'name': u'avant'},
-                    {'x': ["après"], 'y': [myres["total"]["apres"]], 'type': 'bar', 'name': u'après'},
-                    {'x': ["impact"], 'y': [myres["total"]["apres"]-myres["total"]["avant"]], 'type': 'bar', 'name': 'impact'}
-                ]
-            ,
-                'layout': {
-                    'title': 'Impact du changement'
-                }
-            },{
-                'data':
-                [{'x' : ["decile {}".format(i)], 'y':[myres["deciles"][i][2]-myres["deciles"][i][1]] , 'type':'bar', 'name' :"decile {}".format(i)} for i in range(len(myres["deciles"]))]
-            ,
-                'layout': {
-                    'title': 'changement par décile'
-                }
-            }
-    else: #Does not run before the first click
-        return None,None
 
 # Generates results for the graphs depending on the simulation on the full population
 nbseuil=4
