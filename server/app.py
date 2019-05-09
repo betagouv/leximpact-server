b9bf52bc5062307b444b212604977e7cfa79757b
Augustin Wenger <magemax@yahoo.com>
2019-05-10 00:32:57 +0200

    Cors update to make API work with React

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import connexion
 import sys
+from flask_cors import CORS
 
 sys.path.append("..")
 
@@ -8,6 +9,7 @@ sys.path.append("..")
 def create_app() -> connexion.apps.flask_app.FlaskApp:
     app = connexion.App("server", specification_dir=".")
     app.add_api("api.yaml")
+    CORS(app.app, origins="*")
     return app
 
 
