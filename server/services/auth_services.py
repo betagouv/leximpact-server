52e475afb3ccb68b1143c3e85492c809f8a8cc36
RogerMoreau <wengerboy@gmail.com>
2019-07-25 00:53:09 +0200

    check_user function

--- server/services/auth_services.py
+++ server/services/auth_services.py
@@ -1,7 +1,17 @@
 from typing import Optional
 from models.user import User, find_user, update_user_token
-from models.jwt import JWT, encode_jwt
+from models.jwt import JWT, encode_jwt, decode_jwt
+from jwt.exceptions import InvalidSignatureError
+from datetime import datetime
 
+def check_user(session,tokenencoded:str) -> bool:
+    checkjwt=JWT()
+    checkjwt.encoded=tokenencoded
+    try:
+        decode_jwt(checkjwt)
+        return find_user(session,checkjwt.decoded["sub"]) and datetime.utcnow().timestamp()<checkjwt.decoded["exp"]
+    except InvalidSignatureError:
+        return False
 
 def login_user(session, email: str) -> str:#Optional[User]:
     user = find_user(session, email)
