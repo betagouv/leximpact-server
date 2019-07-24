aafdb0e9ec3d349d08d778b27b669041b5ba8e04
RogerMoreau <wengerboy@gmail.com>
2019-07-25 00:52:49 +0200

    function check_user was created.  login_user now returns the jwt (it doesn't update db anymore).

--- server/handlers/auth.py
+++ server/handlers/auth.py
@@ -4,7 +4,8 @@ from server.services import login_user, with_session
 
 @with_session
 def login(session, **params: Dict[str, str]) -> Tuple[str, int]:
-    if login_user(session, params["body"]["email"]):
-        return "", 201
+    jwt=login_user(session, params["body"]["email"])
+    if jwt is not None:
+        return str(token.encoded), 201
 
     return "", 401
--- server/services/auth_services.py
+++ server/services/auth_services.py
@@ -3,11 +3,9 @@ from models.user import User, find_user, update_user_token
 from models.jwt import JWT, encode_jwt
 
 
-def login_user(session, email: str) -> Optional[User]:
+def login_user(session, email: str) -> str:#Optional[User]:
     user = find_user(session, email)
 
     if not user:
         return None
-
-    token = encode_jwt(JWT(), user.email).encoded
-    return update_user_token(session, user.email, token)
+    return encode_jwt(JWT(), user.email)
