c7a1d9e73a163642bd44d5104b0bb9fe59c313ff
Augustin Wenger <magemax@yahoo.com>
2019-07-25 18:27:53 +0200

    login call now returns the same response whether the email exists in database or not.  If the email exists within the database, it sends an email.

--- server/handlers/auth.py
+++ server/handlers/auth.py
@@ -1,11 +1,11 @@
 from typing import Dict, Tuple
-from server.services import login_user, with_session
+from server.services import login_user, with_session,send_mail
 
 
 @with_session
 def login(session, **params: Dict[str, str]) -> Tuple[str, int]:
     jwt=login_user(session, params["body"]["email"])
     if jwt is not None:
-        return str(token.encoded), 201
-
-    return "", 401
+        mail_content="<a href=https://leximpact.beta.gouv.fr/{}>Here's your link my friend. Why am I speaking english?</a>".format(jwt.encoded)
+        send_mail(recipient=params["body"]["email"],subject="your magic link or token",content=mail_content)
+    return "Bien reçu! Si l'email est valide, nous avons envoyé un mail de confirmation", 200
