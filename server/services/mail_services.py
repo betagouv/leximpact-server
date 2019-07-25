0458b0371bb22b6d8306138e8b3429964170d69c
Augustin Wenger <magemax@yahoo.com>
2019-07-25 18:26:13 +0200

    Introducing send_mail function : to send emails through Mailjet (requires a user pass and key to be put in environment)

---
+++ server/services/mail_services.py
@@ -0,0 +1,40 @@
+
+from mailjet_rest import Client
+import os
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+me = "wengerboy+mj@gmail.com"
+you = "magemax@yahoo.com"
+
+load_dotenv(dotenv_path=".env")
+HOST = "in-v3.mailjet.com"
+PORT = 587
+USER = getenv("MAILJET_USER")
+PASSWD = getenv("MAILJET_PASSWD")
+
+
+def send_mail(recipient,subject,content="<h3>Ouaf ouaf!</h3><br />",sender_address=me,myname="Leximpact"):
+        mailjet = Client(auth=(USER, PASSWD), version='v3.1')
+        data = {
+        'Messages': [
+                        {
+                                "From": {
+                                        "Email": sender_address,
+                                        "Name": myname
+                                },
+                                "To": [
+                                        {
+                                           "Email": recipient
+                                        }
+                                ],
+                                "Subject": subject,
+                                "TextPart": "",
+                                "HTMLPart": content
+                        }
+                ]
+        }
+        result = mailjet.send.create(data=data)
+        print(result.status_code)
+        print(result.json())
