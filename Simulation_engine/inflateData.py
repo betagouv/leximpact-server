77b93d872d1913ee66bf081a0f6599041e7b41ef
Augustin Wenger <magemax@yahoo.com>
2019-07-29 18:05:09 +0200

    Création d'une fonction permettant l'ajout de bruit gaussien dans les dimensions chiffrées des données de base

--- Simulation_engine/inflateData.py
+++ Simulation_engine/inflateData.py
@@ -1,5 +1,6 @@
-import pandas as pd  # type: ignore
 
+import pandas as pd  # type: ignore
+import numpy as np
 
 def inflate(inputfile, outputfile=None):
     if outputfile is None:
@@ -33,5 +34,38 @@ def inflate(inputfile, outputfile=None):
     final.to_hdf(outputfile, key="input")
 
 
+def noise(inputfile, outputfile=None): #add gaussian noise
+    if outputfile is None:
+        outputfile = inputfile
+    df = pd.read_hdf(inputfile)
+    columns = df.columns
+    print(columns)
+    print(df)
+    # list of variables to gaussiannoise
+    to_noise = [
+        "chomage_brut",
+        "pensions_alimentaires_percues",
+        "rag",
+        "ric",
+        "rnc",
+        "salaire_de_base",
+        "f4ba",
+        "loyer",
+        "taxe_habitation",
+    ]
+    sigma = 0.02
+
+    for var_noised in to_noise:
+        print("noising {}".format(var_noised))
+        noise = np.random.normal(mu, sigma, [2,2]) 
+        sig=df[var_noised]
+        noise=np.random.lognormal(-sigma*sigma/2,sigma,[len(df)])
+        adjed=sig*noise
+        print(sum(sig),sum(noise)/len(noise),sum(adjed))
+        df[var_noised]=adjed
+    df.to_hdf(outputfile, key="input")
+
+
 if __name__ == "__main__":
     inflate("dummy_data.h5", "dummy_data_inflated.h5")
+    noise("dummy_data_inflated.h5", "dummy_data_inflated_noised.h5")
