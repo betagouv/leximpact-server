719728bb4a73b12929cd5712644dbfa98afc343b
Augustin Wenger <magemax@yahoo.com>
2019-07-30 18:41:27 +0200

    removal of token column in "user" form

--- models/user.py
+++ models/user.py
@@ -8,7 +8,6 @@ Base: Any = declarative_base()
 class User(Base):
     __tablename__ = "users"
     email = Column(String(), primary_key=True)
-    token = Column(String())
 
 
 def find_user(session, email: str) -> Optional[User]:
@@ -20,13 +19,3 @@ def create_user(session, user: User) -> bool:
     session.commit()
     return True
 
-
-def update_user_token(session, email: str, token: str) -> Optional[User]:
-    user = find_user(session, email)
-
-    if user:
-        user.token = token
-        session.commit()
-        return user
-    else:
-        return None
