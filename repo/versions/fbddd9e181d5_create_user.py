a4359cd7cb814f0a354290e44c2422aedf7d6420
Augustin Wenger <magemax@yahoo.com>
2019-07-30 18:35:27 +0200

    update of tables

--- repo/versions/fbddd9e181d5_create_user.py
+++ repo/versions/fbddd9e181d5_create_user.py
@@ -6,7 +6,7 @@ Create Date: 2019-07-18 14:24:41.319556
 
 """
 from alembic.op import create_table, drop_table  # type: ignore
-from sqlalchemy import Column, String  # type: ignore
+from sqlalchemy import Column, String,DateTime,Integer  # type: ignore
 
 
 # revision identifiers, used by Alembic.
@@ -18,9 +18,16 @@ depends_on = None
 
 def upgrade():
     create_table(
-        "users", Column("email", String(), primary_key=True), Column("token", String())
+        "users", Column("email", String(), primary_key=True)
+    )
+    create_table(
+        "requests",
+        Column("id",Integer,primary_key=True),
+        Column("email",String()),
+        Column("timestamp",DateTime())
     )
 
 
 def downgrade():
     drop_table("users")
+    drop_table("requests")
