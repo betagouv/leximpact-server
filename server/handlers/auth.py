07a3302c0581094875a546c4069e6f70912f5c1f
Augustin Wenger <magemax@yahoo.com>
2019-08-05 10:57:34 +0200

    Changed link addressing and Magic Link email content

--- server/handlers/auth.py
+++ server/handlers/auth.py
@@ -6,7 +6,7 @@ from server.services import login_user, check_user,with_session,send_mail
 def login(session, **params: Dict[str, str]) -> Tuple[str, int]:
     jwt=login_user(session, params["body"]["email"])
     if jwt is not None:
-        mail_content="<a href=https://leximpact.beta.gouv.fr/{}>Here's your link my friend. Why am I speaking english?</a>".format(jwt.encoded)
+        mail_content="Votre adresse e-mail est valide, voici votre <a href=https://leximpact.beta.gouv.fr/connection/{}>lien vers Leximpact POP</a>".format(jwt.encoded)
         send_mail(recipient=params["body"]["email"],subject="your magic link or token",content=mail_content)
     return "Bien reçu! Si l'email est valide, nous avons envoyé un mail de confirmation", 200
 
