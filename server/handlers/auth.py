acf90e909e5a6616a6a72af96104475d6d053d40
Augustin Wenger <magemax@yahoo.com>
2019-08-29 17:04:27 +0200

    Not checking anymore if a user is in the DB once they have a token (cause not all emails are in the db). Adding the domains conditions for collabs.

--- server/handlers/auth.py
+++ server/handlers/auth.py
@@ -18,7 +18,6 @@ def login(session, **params: Dict[str, str]) -> Tuple[str, int]:
         mail_content = mail_content_initial.replace(
             "$INSERT_LINK_HERE",
             "https://leximpact.beta.gouv.fr/connection/{}".format(jwt.encoded),
-
         )
         send_mail(
             recipient=params["body"]["email"],
