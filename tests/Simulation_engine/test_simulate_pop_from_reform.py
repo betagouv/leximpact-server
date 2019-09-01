879cce28ee1720c478ec76b7a8ca5e6cfc97ff40
sandcha <sandcha@users.noreply.github.com>
2019-09-01 12:14:25 +0200

    Init test on dataframe_from_cas_types_description

--- tests/Simulation_engine/test_simulate_pop_from_reform.py
+++ tests/Simulation_engine/test_simulate_pop_from_reform.py
@@ -1,10 +1,12 @@
 import pandas  # type: ignore
+from pandas.util.testing import assert_frame_equal
 
 from Simulation_engine.simulate_pop_from_reform import (  # type: ignore
     load_data,
     adjustment,
     adjust_total,
     adjust_deciles,
+    dataframe_from_cas_types_description,
 )
 
 
@@ -40,3 +42,24 @@ def test_adjust_deciles():
     expected = [{"poids": 2, "avant": 4, "apres": 6}]
 
     assert actual == expected
+
+
+def test_dataframe_from_cas_types_description():
+    cas_types_description = {}
+
+    expected_data_columns = ["index", "activite", "age", "categorie_salarie", "chomage_brut", "chomage_imposable", 
+        "contrat_de_travail", "date_naissance", "effectif_entreprise", "heures_remunerees_volume", 
+        "idfam", "idfoy", "idmen", "pensions_alimentaires_percues", "quifam", "quifoy", "quimen", "rag", 
+        "retraite_brute", "ric", "rnc", "statut_marital", "salaire_de_base", "taux_csg_remplacement", 
+        "f4ba", "loyer", "statut_occupation_logement", "taxe_habitation", "wprm", "zone_apl", "quimenof", 
+        "residence_fiscale_guadeloupe", "residence_fiscale_guyane", "quifoyof", "quifamof", 
+        "caseL", "caseT", "caseW", "garde_alternee", "invalidite"]
+    expected_simulation_data = pandas.DataFrame(
+        columns = expected_data_columns, 
+        index = pandas.RangeIndex(start=0, stop=0, step=1),
+        dtype = 'float64') 
+
+    simulation_data = dataframe_from_cas_types_description(cas_types_description)
+    print("expected_simulation_data", expected_simulation_data)
+    print("simulation_data", simulation_data)
+    assert_frame_equal(simulation_data, expected_simulation_data)
