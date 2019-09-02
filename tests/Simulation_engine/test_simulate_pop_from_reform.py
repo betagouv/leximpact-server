92b14d64861a666a2cf3ed2bd735dea948761e5d
sandcha <sandcha@users.noreply.github.com>
2019-09-02 15:59:31 +0200

    Init test on dataframebrutevals

--- tests/Simulation_engine/test_simulate_pop_from_reform.py
+++ tests/Simulation_engine/test_simulate_pop_from_reform.py
@@ -1,11 +1,13 @@
 import pandas  # type: ignore
-from pandas.util.testing import assert_frame_equal
+from pandas.util.testing import assert_frame_equal  # type: ignore
+import numpy as np  # type: ignore
 
 from Simulation_engine.simulate_pop_from_reform import (  # type: ignore
     load_data,
     adjustment,
     adjust_total,
     adjust_deciles,
+    dataframebrutevals,
     dataframe_from_cas_types_description,
 )
 
@@ -45,21 +47,40 @@ def test_adjust_deciles():
 
 
 def test_dataframe_from_cas_types_description():
-    cas_types_description = {}
+    cas_types_description = {}  # Cf. desc_cas_types()
 
-    expected_data_columns = ["index", "activite", "age", "categorie_salarie", "chomage_brut", "chomage_imposable", 
-        "contrat_de_travail", "date_naissance", "effectif_entreprise", "heures_remunerees_volume", 
-        "idfam", "idfoy", "idmen", "pensions_alimentaires_percues", "quifam", "quifoy", "quimen", "rag", 
-        "retraite_brute", "ric", "rnc", "statut_marital", "salaire_de_base", "taux_csg_remplacement", 
-        "f4ba", "loyer", "statut_occupation_logement", "taxe_habitation", "wprm", "zone_apl", "quimenof", 
-        "residence_fiscale_guadeloupe", "residence_fiscale_guyane", "quifoyof", "quifamof", 
+    expected_data_columns = [
+        "index", "activite", "age", "categorie_salarie", "chomage_brut", "chomage_imposable",
+        "contrat_de_travail", "date_naissance", "effectif_entreprise", "heures_remunerees_volume",
+        "idfam", "idfoy", "idmen", "pensions_alimentaires_percues", "quifam", "quifoy", "quimen", "rag",
+        "retraite_brute", "ric", "rnc", "statut_marital", "salaire_de_base", "taux_csg_remplacement",
+        "f4ba", "loyer", "statut_occupation_logement", "taxe_habitation", "wprm", "zone_apl", "quimenof",
+        "residence_fiscale_guadeloupe", "residence_fiscale_guyane", "quifoyof", "quifamof",
         "caseL", "caseT", "caseW", "garde_alternee", "invalidite"]
     expected_simulation_data = pandas.DataFrame(
-        columns = expected_data_columns, 
-        index = pandas.RangeIndex(start=0, stop=0, step=1),
-        dtype = 'float64') 
+        columns=expected_data_columns,
+        index=pandas.RangeIndex(start=0, stop=0, step=1),
+        dtype='float64')
 
     simulation_data = dataframe_from_cas_types_description(cas_types_description)
     print("expected_simulation_data", expected_simulation_data)
     print("simulation_data", simulation_data)
     assert_frame_equal(simulation_data, expected_simulation_data)
+
+
+def test_dataframebrutevals():
+    revenus_bruts = 19219.327646
+    revenus_imposables = 15600
+
+    expected_data_columns = [revenus_bruts, "idfam", "idfoy", "idmen", "quifam", "quifoy", "quimen"]
+    expected_data = np.array([revenus_imposables, 0, 0, 0, 0, 0, 0], dtype='int64')
+    expected = pandas.DataFrame(
+        [expected_data],
+        columns=expected_data_columns)
+
+    maxv = 25405
+    pourcentage_hausse = 11
+    valeur_hausse = pourcentage_hausse * revenus_bruts / 100
+    revenus_bruts = dataframebrutevals(revenus_bruts, revenus_imposables, maxv, pourcentage_hausse, valeur_hausse)
+
+    assert_frame_equal(revenus_bruts, expected)
