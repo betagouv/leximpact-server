4dde7ae9657c19360deb3f6be17018d47cf0cb66
Augustin Wenger <magemax@yahoo.com>
2019-09-13 03:53:08 +0200

    Creation of request_results table; introducing functions to manipulate them in the db

--- models/__init__.py
+++ models/__init__.py
@@ -11,3 +11,4 @@ from .user import User, find_user, create_user  # noqa
 from .ETLpostgres import from_postgres, to_postgres  # noqa
 from .request import create_request, count_requests  # noqa
 from .suspended import create_suspension, count_active_suspensions  # noqa
+from .request_result import create_request_result, get_request_result, update_request_result
---
+++ models/request_result.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, String, DateTime, Integer  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from typing import Any
+import datetime
+
+Base: Any = declarative_base()
+
+
+class RequestResult(Base):
+    __tablename__ = "request_result"
+    id_request = Column(String())
+    status = Column(String())
+    result = Column(String())
+
+
+def create_request_result(session, id_req):
+    request = RequestResult(id_request=id_req, status="created")
+    session.add(request)
+    session.commit()
+    return True
+
+def get_request_result(session, id_req):
+    return session.query(RequestResult).get(id_req)
+
+def update_request_result(session, new_status, new_result = None) -> Optional[RequestResult]:
+    request_result = get_request_result(session, email)
+    if request_result is not None:
+        request_result.status = new_status
+        if new_result is not None:
+            request_result.result = new_result
+        session.commit()
+        return request_result
+    else:
+        return None
