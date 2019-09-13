ead7e31c588364c1b09e49cf24c07d7de927820a
Augustin Wenger <magemax@yahoo.com>
2019-09-13 03:53:08 +0200

    Creation of request_results table

--- repo/versions/087830d21c00_add_result_to_requests_table.py
+++ repo/versions/087830d21c00_add_result_to_requests_table.py
@@ -5,8 +5,8 @@ Revises: 700a389cb5e1
 Create Date: 2019-08-28 15:23:19.645237
 
 """
-from alembic.op import add_column  # type: ignore
-from sqlalchemy import Column, String  # type: ignore
+from alembic.op import add_column,drop_column,create_table,drop_table  # type: ignore
+from sqlalchemy import Column, String, Integer  # type: ignore
 
 
 
@@ -18,7 +18,12 @@ depends_on = None
 
 
 def upgrade():
-    add_column("requests", Column("result", String(), server_default="running"))
+    create_table(
+        "request_results",
+        Column("id_request", Integer, primary_key=True),
+        Column("status", String()),
+        Column("result", String(), server_default="{}"),
+    )
 
 def downgrade():
-    drop_column("requests", "result")
+    drop_table("request_results")
