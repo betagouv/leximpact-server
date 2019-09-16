80285ce45eeea173fbef08215024fd6df5638605
sandcha <sandcha@users.noreply.github.com>
2019-09-16 18:59:38 +0200

    Add token tests

--- tests/server/handlers/test_async.py
+++ tests/server/handlers/test_async.py
@@ -1,53 +1,61 @@
 from datetime import datetime
 from functools import partial
-from http.client import OK
+from http.client import OK, BAD_REQUEST
+
+from dotenv import load_dotenv
+from os import getenv
 
 import pytest  # type: ignore
 import dpath
 import json
 
 
-def payload() -> dict:
-    return {
-        "reforme": {
-            "impot_revenu": {
-                "bareme": {
-                    "seuils": [9964, 27519, 73779, 156244],
-                    "taux": [0.12, 0.30, 0.41, 0.45],
+load_dotenv(dotenv_path=".env")
+TEST_TOKEN = getenv("TEST_TOKEN")
+
+reform_payload = {
+    "reforme": {
+        "impot_revenu": {
+            "bareme": {
+                "seuils": [9964, 27519, 73779, 156244],
+                "taux": [0.12, 0.30, 0.41, 0.45],
+            },
+            "decote": {"seuil_celib": 1196, "seuil_couple": 1970, "taux": 50},
+            "plafond_qf": {
+                "abat_dom": {
+                    "taux_GuadMarReu": 0.3,
+                    "plaf_GuadMarReu": 2450,
+                    "taux_GuyMay": 0.4,
+                    "plaf_GuyMay": 4050,
                 },
-                "decote": {"seuil_celib": 1196, "seuil_couple": 1970, "taux": 50},
-                "plafond_qf": {
-                    "abat_dom": {
-                        "taux_GuadMarReu": 0.3,
-                        "plaf_GuadMarReu": 2450,
-                        "taux_GuyMay": 0.4,
-                        "plaf_GuyMay": 4050,
-                    },
-                    "maries_ou_pacses": 1551,
-                    "celib_enf": 3660,
-                    "celib": 927,
-                    "reduc_postplafond": 1547,
-                    "reduc_postplafond_veuf": 1728,
-                    "reduction_ss_condition_revenus": {
-                        "seuil_maj_enf": 3797,
-                        "seuil1": 18984,
-                        "seuil2": 21036,
-                        "taux": 0.20,
-                    },
+                "maries_ou_pacses": 1551,
+                "celib_enf": 3660,
+                "celib": 927,
+                "reduc_postplafond": 1547,
+                "reduc_postplafond_veuf": 1728,
+                "reduction_ss_condition_revenus": {
+                    "seuil_maj_enf": 3797,
+                    "seuil1": 18984,
+                    "seuil2": 21036,
+                    "taux": 0.20,
                 },
-                "abattements_rni": {
-                    "personne_agee_ou_invalide": {
-                        "montant_1": 1,
-                        "montant_2": 2,
-                        "plafond_1": 3,
-                        "plafond_2": 4,
-                    }
-                },
-            }
-        },
-        "deciles": False,
-        "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f"),
-    }
+            },
+            "abattements_rni": {
+                "personne_agee_ou_invalide": {
+                    "montant_1": 1,
+                    "montant_2": 2,
+                    "plafond_1": 3,
+                    "plafond_2": 4,
+                }
+            },
+        }
+    },
+    "deciles": False,
+    "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f"),
+}
+
+reform_payload_empty_token = dict(reform_payload, token='')
+reform_payload_with_token = dict(reform_payload, token=TEST_TOKEN)
 
 # /calculate/simpop_async
 # server.handlers.SimulationRunner.simuledeciles_async
@@ -68,7 +76,7 @@ def post_request(client, endpoint, headers):
 
 def check_response(actual_result, expected_status_code, content_path_to_check, content_to_check):
     if 'status' in actual_result:
-        assert actual_result.status == expected_status_code
+        assert actual_result['status'] == expected_status_code
     
     if content_path_to_check:
         content = dpath.util.get(actual_result, content_path_to_check)
@@ -76,12 +84,13 @@ def check_response(actual_result, expected_status_code, content_path_to_check, c
 
 
 @pytest.mark.parametrize("payload, expected_status_code, content_path_to_check, content_to_check", [
-    (payload(), OK, 'Error', 'missing token: necessary for this request')
+    (None, BAD_REQUEST, 'detail', 'Request body is not valid JSON'),
+    (reform_payload, OK, 'Error', 'missing token: necessary for this request'),  # Token signature was invalid
+    (reform_payload_empty_token, BAD_REQUEST, 'Error', 'Token invalid : not Decodable'),
+    (reform_payload_with_token, OK, 'Error', 'Token signature was invalid')
     ])
 def test_calculate_simpop_async(client, payload, headers, expected_status_code, content_path_to_check, content_to_check):
     partial_response = post_request(client, "calculate/simpop_async", headers)
     actual_result = json.loads(partial_response(data=json.dumps(payload)).data)
     
     check_response(actual_result, expected_status_code, content_path_to_check, content_to_check)
-
-
