db6e34929ab622e73e1f765c2d5c32776f894526
sandcha <sandcha@users.noreply.github.com>
2019-09-18 16:57:13 +0200

    Import global TBS in warmup endpoint

--- server/handlers/warmup.py
+++ server/handlers/warmup.py
@@ -1,9 +1,7 @@
-from openfisca_france import FranceTaxBenefitSystem  # type: ignore
-
 
 class WarmUp(object):
     def load_tax_benefits_system(**params: dict) -> tuple:
-        tax_benefits_system = FranceTaxBenefitSystem()
-        version = tax_benefits_system.get_package_metadata()['version']
-
+        # import the TBS at handler call only
+        from Simulation_engine.simulate_pop_from_reform import TBS
+        version = TBS.get_package_metadata()['version']
         return {"status": f"A chargé en mémoire OpenFisca-France v{version}"}, 200
