b9a198729c19ffbc9d732f77295e3d5925d506f6
sandcha <sandcha@users.noreply.github.com>
2019-09-18 16:53:00 +0200

    Fix warmup endpoint definition

--- server/handlers/warmup.py
+++ server/handlers/warmup.py
@@ -1,6 +1,9 @@
 from openfisca_france import FranceTaxBenefitSystem  # type: ignore
 
+
 class WarmUp(object):
-    def load_taxbenefits_system(**params: dict) -> tuple:
-        TBS = FranceTaxBenefitSystem()
-        return {"Status": "A chargé en mémoire OpenFisca-France vX"}, 200
+    def load_tax_benefits_system(**params: dict) -> tuple:
+        tax_benefits_system = FranceTaxBenefitSystem()
+        version = tax_benefits_system.get_package_metadata()['version']
+
+        return {"status": f"A chargé en mémoire OpenFisca-France v{version}"}, 200
