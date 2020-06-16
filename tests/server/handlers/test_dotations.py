738a45441ad2ced17486a1c7da26f154a9e97f84
sandcha <sandcha@users.noreply.github.com>
2020-06-16 15:49:45 +0200

    Detect in test dotations request body error

--- tests/server/handlers/test_dotations.py
+++ tests/server/handlers/test_dotations.py
@@ -2,10 +2,11 @@ from functools import partial
 import json
 
 
-def test_dotations(client, headers):
+def test_dotations_request_body_error(client, headers):
     request = {}
 
     response_function = partial(client.post, "dotations", headers=headers)
     response = response_function(data=json.dumps(request))
 
     assert response.status_code == 400
+    assert "Error" in json.loads(response.data)
