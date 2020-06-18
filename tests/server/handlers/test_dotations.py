9468ac77b148e195d92f7bea59735aaa23d10305
sandcha <sandcha@users.noreply.github.com>
2020-06-18 09:24:06 +0200

    Add success test on dotations endpoint

--- tests/server/handlers/test_dotations.py
+++ tests/server/handlers/test_dotations.py
@@ -10,3 +10,14 @@ def test_dotations_request_body_error(client, headers):
 
     assert response.status_code == 400
     assert "Error" in json.loads(response.data)
+
+
+def test_dotations(client, headers):
+    request = {
+        "dotations": {}
+    }
+
+    response_function = partial(client.post, "dotations", headers=headers)
+    response = response_function(data=json.dumps(request))
+
+    assert response.status_code == 200
