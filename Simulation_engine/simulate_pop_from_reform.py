9e247fa5f70d0789f799556aa98def1fc5b01cf6
Augustin Wenger <magemax@yahoo.com>
2020-06-24 18:03:34 +0200

    Removes wprm from res_brut and nbreParts

--- Simulation_engine/simulate_pop_from_reform.py
+++ Simulation_engine/simulate_pop_from_reform.py
@@ -291,13 +291,17 @@ def compare(period: str, dictionnaire_simulations, compute_deciles=True):
     else:  # This only interests us for the castypes
         # On arrondit les résultats des cas-types
         dic_res_brut = impots_par_reforme.to_dict()
+        del dic_res_brut["wprm"]
         for simu in dic_res_brut:
-            for cas_type in dic_res_brut[simu]:
-                dic_res_brut[simu][cas_type] = int(round(dic_res_brut[simu][cas_type]))
+            if simu != "wprm":
+                for cas_type in dic_res_brut[simu]:
+                    dic_res_brut[simu][cas_type] = int(round(dic_res_brut[simu][cas_type]))
         dic_nbptr = nbptr_par_reforme.to_dict()
+        del dic_nbptr["wprm"]
         for simu in dic_nbptr:
-            for cas_type in dic_nbptr[simu]:
-                dic_nbptr[simu][cas_type] = dic_nbptr[simu][cas_type]
+            if simu != "wprm":
+                for cas_type in dic_nbptr[simu]:
+                    dic_nbptr[simu][cas_type] = dic_nbptr[simu][cas_type]
         resultat = {"total": total, "res_brut": dic_res_brut, "nbreParts": dic_nbptr}
 
     return resultat
