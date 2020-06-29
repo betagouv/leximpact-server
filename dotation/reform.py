d5a72b654aaeb2e13f184772c4a593cd066e31fc
Augustin Wenger <magemax@yahoo.com>
2020-06-29 22:22:54 +0200

    Now parsing specific fields in the reform request with ad hoc methods to enable to modify a Scale in a reform.

--- dotation/reform.py
+++ dotation/reform.py
@@ -1,10 +1,24 @@
 from openfisca_core import periods  # type: ignore
 from openfisca_core.reforms import Reform  # type: ignore
-from openfisca_core.parameters import ParameterNode  # type: ignore
+from openfisca_core.parameters import ParameterNode, Bracket  # type: ignore
 from functools import reduce
 
 from dotation.utils_dict import flattened_dict  # type: ignore
 
+
+def set_brackets(brackets, target):
+    # target est un array donné sous la forme [{"threshold": threshold[0], "amount": amount[0]), ... , {"threshold": threshold[-1], "amount": amount[-1]}]
+    while brackets:
+        del brackets[0]
+    for params_bracket in target:
+        new_bracket = Bracket(
+            'new_bracket',
+            data = {
+                key: {'1900-01-01': {'value': value}} for key,value in params_bracket.items()
+                },
+            )
+        brackets.append(new_bracket)
+        
 
 class DotationReform(Reform):
 
@@ -17,10 +31,25 @@ class DotationReform(Reform):
         # passe en revue tous les champs du dictionnaire de réforme de payload.
         # Ceux qui ne sont pas des dictionnaires seront mis à ce niveau dans ofdl
         flatpayload = flattened_dict(self.payload)
+
+
+        # Quand des champs complexes entrent en compte, un traitement ad hoc est nécessaire pour ces champs
+        # pour éviter qu'ils passent dans la moulinette d'en bas.
+        # Schéma pour les variables qui ne correspondent pas à un chemin OFDL
+        # par exemple les brackets ou les variables où des calculs sont nécessaires :
+        # Une fonction ad hoc de modification des paramètres est créée et la clef est retirée pour éviter
+        # de soulever une erreur à l'étape suivante
+        if "population.plafond_dgf" in flatpayload:
+            set_brackets(parameters.population.plafond_dgf.brackets, flatpayload["population.plafond_dgf"])
+            del flatpayload["population.plafond_dgf"]
+
+        # Variables correspondant à un chemin OFDL : Tous les éléments de la réforme qui n'ont
+        # pas été retirés précédemment  vont être directement fixés à un niveau dans les parametres
+        # Ca raisera les mêmes erreurs que parameters.mon.chemin.qui.nexiste.pas.update(valeur) si le 
+        # chemin n'existe pas 
+    
         for field_to_update, value in flatpayload.items():
             reduce(getattr, field_to_update.split("."), parameters).update(period=self.period, value=value)
-        # Quand des champs complexes entreront en compte, un traitement ad hoc sera nécessaire pour ces champs
-        # et pour éviter qu'ils passent dans la moulinette ci-dessus
 
         return parameters
 
