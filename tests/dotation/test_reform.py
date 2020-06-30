d725f8b8a59d15b369e6c5203e267f5b1323cd17
Augustin Wenger <magemax@yahoo.com>
2020-06-30 17:34:08 +0200

    styling / typing

--- tests/dotation/test_reform.py
+++ tests/dotation/test_reform.py
@@ -1,13 +1,12 @@
-from dotation.reform import DotationReform
+from dotation.reform import DotationReform  # type: ignore
 from openfisca_france_dotations_locales import CountryTaxBenefitSystem  # type: ignore
-
 
 
 def test_correct_reform_applied():
     tbs = CountryTaxBenefitSystem()
     value_to_set = 793
     description_reforme = {
-        "dgf":{
+        "dgf": {
             "dotation_solidarite_rurale": {
                 "cible": {
                     "eligibilite": {
@@ -19,23 +18,4 @@ def test_correct_reform_applied():
     }
     tbs_modified = DotationReform(tbs, description_reforme)
 
-    print(tbs_modified.get_parameters_at_instant("2020-01-01")["dotation_solidarite_rurale"]["cible"]["eligibilite"]["seuil_classement"]==793)
-
-def test_modif_nothing():
-    tbs = CountryTaxBenefitSystem()
-    value_to_set = 793
-    description_reforme = {
-        "dgf":{
-            "nimportequoi": {
-                "anythinggoes": {
-                    "memepasdedashcase": 168
-                }
-            }
-        }
-    }
-    tbs_modified = DotationReform(tbs, description_reforme)
-
-    print(tbs_modified.get_parameters_at_instant("2020-01-01")==tbs_modified.get_parameters_at_instant("2020-01-01"))
-
-
-test_modif_nothing()
+    print(tbs_modified.get_parameters_at_instant("2020-01-01")["dotation_solidarite_rurale"]["cible"]["eligibilite"]["seuil_classement"] == value_to_set)
