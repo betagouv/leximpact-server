8c6ea6c8e699000537711378ea1972817b79671c
Augustin Wenger <magemax@yahoo.com>
2020-07-01 23:30:52 +0200

    Links API response to dotation engine.

--- Simulation_engine/simulate_dotations.py
+++ Simulation_engine/simulate_dotations.py
@@ -1,5 +1,6 @@
+from dotation.impact import impacts_reforme_dotation, format_reforme_openfisca  # type: ignore
 
 
 def simulate(request_body):
-    simulation_result = {"hello": "coucou"}  # construire le body de la response dans le handler ?
+    simulation_result = impacts_reforme_dotation(format_reforme_openfisca(request_body["reforme"]))
     return simulation_result
