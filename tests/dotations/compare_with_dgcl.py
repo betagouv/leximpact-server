aa546def88ada8c71126b833462ae5c6a9d201b4
Augustin Wenger <magemax@yahoo.com>
2020-07-15 18:23:33 +0200

    Adds a comparison function to compare our results with precomputed results in the case of a column that take number values

--- tests/dotations/compare_with_dgcl.py
+++ tests/dotations/compare_with_dgcl.py
@@ -37,6 +37,47 @@ def check_variables_bool(data):
     return res
 
 
+# Columns to compare that contain a number
+# There will be great output, e.g. :
+# Absolute differences : < 1 €, quantiles extrèmes, deciles de différence
+# Différence relative (osef un peu)
+# Erreur quadratique
+# Norme L1, L2,
+def compare_results_real(data, nom_actual, nom_expected):
+    res = {}
+    data_non_nul = data[(data[nom_actual] != 0) | data[nom_expected] != 0]
+    diff = sorted((data_non_nul[nom_actual] - data_non_nul[nom_expected]).tolist())
+    nb_non_nul = len(data_non_nul)
+    avg_size = sum(data_non_nul[nom_actual]) / nb_non_nul
+    avg_size2 = sum([i * i for i in data_non_nul[nom_actual]]) / (nb_non_nul - 1)
+    res["Moyenne base"] = avg_size
+    res["variance"] = avg_size2 - avg_size * avg_size
+
+    # Norme L1 : ecart absolu moyen
+    res["L1"] = sum([abs(dif) for dif in diff]) / nb_non_nul
+    # Norme L2 (norme euclidienne) : utile pour l'estimation
+    res["L2"] = (sum([dif * dif for dif in diff]) / nb_non_nul)**0.5
+    # Ah ben qu'est ce que je disais : la norme L2 permet de regarder
+    # quelle part de variance de la variable est expliquée par notre
+    # modèle
+    res["pourcentage expliqué"] = 1 - res["L2"]**2 / res["variance"]
+    # Différence maximale. Sert pas à grand chose.
+    res["L∞"] = max([abs(dif) for dif in diff])
+    quantiles = [0.01, 0.02, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.98, 0.99]
+    # Statistiques d'ordre sur les différences (min, max et quantiles)
+    res["min"] = min(diff)
+    res["max"] = max(diff)
+    res["quantiles"] = {}
+    for q in quantiles:
+        rang = int(q * (nb_non_nul - 1) + 0.5)
+        res["quantiles"][q] = (rang, diff[rang])
+    tolerance_difference = 1
+
+    # nombre de résultats considérés comme égaux (en fonction de la tolérance acceptable)
+    res["differents"] = len([d for d in diff if abs(d) > tolerance_difference])
+    res["identiques"] = nb_non_nul - res["differents"]
+    return res
+
 def print_eligible_comparison():
     PERIOD = "2020"
     DATA = adapt_dgcl_data(load_dgcl_file())
