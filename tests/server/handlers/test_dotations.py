ac2f43508342ddc879867758d59e5410c463c2bc
sandcha <sandcha@users.noreply.github.com>
2020-07-21 09:23:05 +0200

    Extract different test_dsr_reform_popMax checks in distinct tests

--- tests/server/handlers/test_dotations.py
+++ tests/server/handlers/test_dotations.py
@@ -5,7 +5,7 @@ from dotations.impact import BORNES_STRATES, get_cas_types_codes_insee  # type: 
 from dotations.utils_dict import flattened_dict  # type: ignore
 
 
-def distance_listes(a, b):
+def _distance_listes(a, b):
     return max([abs(x - y) for (x, y) in zip(a, b)])
 
 
@@ -35,7 +35,7 @@ def test_dotations(client, headers):
     assert response.status_code == 200
 
 
-def test_dsr_reform_popMax(client, headers):
+def test_dsr_reform_eligibilite_montants(client, headers):
     request = {
         "reforme": {
             "dotations": {
@@ -91,40 +91,8 @@ def test_dsr_reform_popMax(client, headers):
 
     base_dsr = result["base"]["dotations"]["communes"]["dsr"]
     amendement_dsr = result["amendement"]["dotations"]["communes"]["dsr"]
-    # même nombre de cas types en loi actuelle et amendement
-    # Les cas_types sont ceux attendus
-    codes_communes = get_cas_types_codes_insee()
-    assert (codes_communes
-            == [cas_type["code"] for cas_type in base_dsr["communes"]]
-            == [cas_type["code"] for cas_type in amendement_dsr["communes"]]
-            )
-
-    assert (len(BORNES_STRATES) - 1) == len(base_dsr["strates"]) == len(amendement_dsr["strates"])
-    assert (BORNES_STRATES[:-1]
-            == [description_strate["habitants"] for description_strate in base_dsr["strates"]]
-            == [description_strate["habitants"] for description_strate in amendement_dsr["strates"]]
-            )
-
-
-    # Vérification des clefs du dictionnaire contenues dans un array :
-    # cas-types
-    expected_cas_type_keys = set(["code", "eligible", "dotationParHab"])
-    for cas_type in base_dsr["communes"] + amendement_dsr["communes"]:
-        assert set(cas_type.keys()) == expected_cas_type_keys
-    # strates
-    expected_strates_keys = set(["eligibles", "habitants", "partPopTotale", "potentielFinancierMoyenParHabitant", "dotationMoyenneParHab", "partDotationTotale"])
-    for strate in base_dsr["strates"] + amendement_dsr["strates"]:
-        assert set(strate.keys()) == expected_strates_keys
 
     # Vérification des valeurs connues :
-    # part des populations des strates
-
-    expected_strates_part_pop = [0.060324, 0.16357, 0.14816, 0.12193, 0.112542, 0.15472, 0.087568, 0.151159]
-    expected_strates_potentiel_financier = [761.7826724474608, 822.7862081792102, 962.3232471567082, 1061.3215646634023, 1142.2386532655016, 1212.5588966550042, 1322.006448304583, 1450.6968113217495]
-    allowed_error = 0.0001
-    for resultat_strates in [base_dsr["strates"], amendement_dsr["strates"]]:
-        assert(distance_listes(expected_strates_part_pop, [strate["partPopTotale"] for strate in resultat_strates]) < allowed_error)
-        assert(distance_listes(expected_strates_potentiel_financier, [strate["potentielFinancierMoyenParHabitant"] for strate in resultat_strates]) < allowed_error)
 
     # Les nombres de communes éligibles de base sont ceux attendus
     expected_strates_eligibilite_base = [17752, 11103, 3165, 1089, 55, 0, 0, 0]
@@ -146,3 +114,154 @@ def test_dsr_reform_popMax(client, headers):
     for scenario_strates in [base_dsr["strates"], amendement_dsr["strates"]]:
         for strate in scenario_strates:
             assert((strate["dotationMoyenneParHab"] > 0) == (strate["eligibles"] > 0))
+
+
+def test_dsr_reform_cas_types(client, headers):
+    request = {
+        "reforme": {
+            "dotations": {
+                "montants": {
+                    "dgf": 31
+                },
+                "communes": {
+                    "dsr": {
+                        "eligibilite": {
+                            "popMax": 500  # de 10 000 à 500
+                        }
+                    }
+                }
+            }
+        }
+    }
+    # avant réforme : +11 000 communes éligibles à la DSR (toutes fractions comprises)
+    expected_reform_impact = {
+        "amendement": {
+            "dotations": {
+                "communes": {
+                    "dsr": {
+                        "communes": [],
+                        "eligibles": 17049,
+                        "nouvellementEligibles": 0,
+                        "plusEligibles": 16115,
+                        "strates": []
+                    }
+                }
+            }
+        },
+        "base": {
+            "dotations": {
+                "communes": {
+                    "dsr": {
+                        "communes": [],
+                        "eligibles": 33164,
+                        "strates": []
+                    }
+                }
+            }
+        }
+    }
+
+    response_function = partial(client.post, "dotations", headers=headers)
+    response = response_function(data=json.dumps(request))
+    result = json.loads(response.data)
+
+    # Vérification des clefs du dictionnaire (sauf celles inclues dans un array)
+    flattened_result_keys = set(flattened_dict(result).keys())
+    flattened_expected_keys = set(flattened_dict(expected_reform_impact).keys())
+    assert flattened_result_keys == flattened_expected_keys
+
+    base_dsr = result["base"]["dotations"]["communes"]["dsr"]
+    amendement_dsr = result["amendement"]["dotations"]["communes"]["dsr"]
+
+    # même nombre de cas types en loi actuelle et amendement
+    # Les cas_types sont ceux attendus
+    codes_communes = get_cas_types_codes_insee()
+    assert (codes_communes
+            == [cas_type["code"] for cas_type in base_dsr["communes"]]
+            == [cas_type["code"] for cas_type in amendement_dsr["communes"]]
+            )
+
+    # Vérification des clefs du dictionnaire contenues dans un array :
+    # cas-types
+    expected_cas_type_keys = set(["code", "eligible", "dotationParHab"])
+    for cas_type in base_dsr["communes"] + amendement_dsr["communes"]:
+        assert set(cas_type.keys()) == expected_cas_type_keys
+
+
+def test_dsr_reform_strates(client, headers):
+    request = {
+        "reforme": {
+            "dotations": {
+                "montants": {
+                    "dgf": 31
+                },
+                "communes": {
+                    "dsr": {
+                        "eligibilite": {
+                            "popMax": 500  # de 10 000 à 500
+                        }
+                    }
+                }
+            }
+        }
+    }
+    # avant réforme : +11 000 communes éligibles à la DSR (toutes fractions comprises)
+    expected_reform_impact = {
+        "amendement": {
+            "dotations": {
+                "communes": {
+                    "dsr": {
+                        "communes": [],
+                        "eligibles": 17049,
+                        "nouvellementEligibles": 0,
+                        "plusEligibles": 16115,
+                        "strates": []
+                    }
+                }
+            }
+        },
+        "base": {
+            "dotations": {
+                "communes": {
+                    "dsr": {
+                        "communes": [],
+                        "eligibles": 33164,
+                        "strates": []
+                    }
+                }
+            }
+        }
+    }
+
+    response_function = partial(client.post, "dotations", headers=headers)
+    response = response_function(data=json.dumps(request))
+    result = json.loads(response.data)
+
+    # Vérification des clefs du dictionnaire (sauf celles inclues dans un array)
+    flattened_result_keys = set(flattened_dict(result).keys())
+    flattened_expected_keys = set(flattened_dict(expected_reform_impact).keys())
+    assert flattened_result_keys == flattened_expected_keys
+
+    base_dsr = result["base"]["dotations"]["communes"]["dsr"]
+    amendement_dsr = result["amendement"]["dotations"]["communes"]["dsr"]
+
+    assert (len(BORNES_STRATES) - 1) == len(base_dsr["strates"]) == len(amendement_dsr["strates"])
+    assert (BORNES_STRATES[:-1]
+            == [description_strate["habitants"] for description_strate in base_dsr["strates"]]
+            == [description_strate["habitants"] for description_strate in amendement_dsr["strates"]]
+            )
+
+    # Vérification des clefs du dictionnaire contenues dans un array :
+    # strates
+    expected_strates_keys = set(["eligibles", "habitants", "partPopTotale", "potentielFinancierMoyenParHabitant", "dotationMoyenneParHab", "partDotationTotale"])
+    for strate in base_dsr["strates"] + amendement_dsr["strates"]:
+        assert set(strate.keys()) == expected_strates_keys
+
+    # Vérification des valeurs connues :
+    # part des populations des strates
+    expected_strates_part_pop = [0.060324, 0.16357, 0.14816, 0.12193, 0.112542, 0.15472, 0.087568, 0.151159]
+    expected_strates_potentiel_financier = [761.7826724474608, 822.7862081792102, 962.3232471567082, 1061.3215646634023, 1142.2386532655016, 1212.5588966550042, 1322.006448304583, 1450.6968113217495]
+    allowed_error = 0.0001
+    for resultat_strates in [base_dsr["strates"], amendement_dsr["strates"]]:
+        assert(_distance_listes(expected_strates_part_pop, [strate["partPopTotale"] for strate in resultat_strates]) < allowed_error)
+        assert(_distance_listes(expected_strates_potentiel_financier, [strate["potentielFinancierMoyenParHabitant"] for strate in resultat_strates]) < allowed_error)
