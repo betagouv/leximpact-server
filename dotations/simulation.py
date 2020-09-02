64e2c49f8c0548f899f369ee8b99a26306e1db75
Augustin Wenger <magemax@yahoo.com>
2020-09-02 16:36:34 +0200

    Determining the non-OFDL required results when it is possible to compute them (depends on whether certain columns are computed in the simulation)

--- dotations/simulation.py
+++ dotations/simulation.py
@@ -80,8 +80,29 @@ def resultfromreforms(dict_ref=None, to_compute_res=("dsr_eligible_fraction_bour
     for nom_scenario, sim in dict_sims.items():
         for champ in to_compute_res:
             DATA[champ + "_" + nom_scenario] = sim.calculate(champ, PERIOD)
+
             if DATA[champ + "_" + nom_scenario].dtype == np.float32:
                 DATA[champ + "_" + nom_scenario] = DATA[champ + "_" + nom_scenario].astype(np.float64)
+
+        # Determination d'une metrique non calculée dans OFDL
+        # Le nombre d'années nécessaires à la convergence de la DSR, métrique
+        # qui passionne le tout Paris d'après les CR d'entretiens utilisateurs
+
+        # La DSR est composée de 3 fractions, chacune d'entre elle ne peut varier que dans une certaine mesure (entre -10% et +20%)
+        # Si tous les champs nécessaires au calcul sont présentes dans les résultats calculés, on inclut
+        # les metrics calculées à la main
+        required_variables = ["dsr_montant_eligible_fraction_bourg_centre", "dsr_montant_eligible_fraction_perequation", "dsr_montant_hors_garanties_fraction_cible", "dsr_montant_hors_garanties_fraction_perequation", "dsr_montant_hors_garanties_fraction_bourg_centre"]
+        required_columns = [k + "_" + nom_scenario for k in required_variables]
+        missing_columns = len([k for k in required_columns if k not in DATA.columns])
+        if not missing_columns:
+            data_convergence = DATA[[k + "_" + nom_scenario for k in ["dsr_montant_eligible_fraction_bourg_centre", "dsr_montant_eligible_fraction_perequation", "dsr_montant_hors_garanties_fraction_cible", "dsr_montant_hors_garanties_fraction_perequation", "dsr_montant_hors_garanties_fraction_bourg_centre"]]]
+            data_convergence["rapport_bc"] = np.where(DATA["dsr_montant_eligible_fraction_bourg_centre" + "_" + nom_scenario] != DATA["dsr_montant_hors_garanties_fraction_bourg_centre" + "_" + nom_scenario], DATA["dsr_montant_hors_garanties_fraction_bourg_centre" + "_" + nom_scenario] / DATA["dsr_montant_eligible_fraction_bourg_centre" + "_" + nom_scenario], 1)
+            data_convergence["rapport_pq"] = np.where(DATA["dsr_montant_eligible_fraction_perequation" + "_" + nom_scenario] != DATA["dsr_montant_hors_garanties_fraction_perequation" + "_" + nom_scenario], DATA["dsr_montant_hors_garanties_fraction_perequation" + "_" + nom_scenario] / DATA["dsr_montant_eligible_fraction_perequation" + "_" + nom_scenario], 1)
+            data_convergence["annee_bc"] = np.where(data_convergence["rapport_bc"] != 1, np.ceil(np.maximum(np.log(data_convergence["rapport_bc"]) / np.log(0.9), np.log(data_convergence["rapport_bc"]) / np.log(1.2))), 0) + 1
+            data_convergence["annee_pq"] = np.where(data_convergence["rapport_pq"] != 1, np.ceil(np.maximum(np.log(data_convergence["rapport_pq"]) / np.log(0.9), np.log(data_convergence["rapport_pq"]) / np.log(1.2))), 0) + 1
+            # print(data_convergence)
+            # data_convergence.to_csv("blondie.csv")
+            DATA["annee_convergence_dsr_" + nom_scenario] = np.maximum(data_convergence["annee_bc"], data_convergence["annee_pq"])
 
     return DATA
 
