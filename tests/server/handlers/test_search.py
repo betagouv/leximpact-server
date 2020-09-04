f29ec1e9598254e8e30b24242d68ce72beb413b8
sandcha <sandcha@users.noreply.github.com>
2020-09-04 13:19:44 +0200

    Add success test on search endpoint

--- tests/server/handlers/test_search.py
+++ tests/server/handlers/test_search.py
@@ -14,6 +14,19 @@ def test_missing_param(client, headers):
     assert "Error" in json.loads(response.data)
 
 
+def test_ok(client, headers):
+    request = {
+        "extraitNomCommune": "paris"
+    }
+
+    response_function = partial(client.post, "search", headers=headers)
+    response = response_function(data=json.dumps(request))
+
+    assert response.status_code == 200
+    response_list = json.loads(response.data)
+    assert len(response_list) > 0
+
+
 def test_empty(client, headers):
     request = {
         "extraitNomCommune": ""
