cb6670974de66c3cfbea1c8b44d8da6ea63f538c
magemax <magemax@yahoo.com>
2020-09-04 15:23:36 +0200

    Apply suggestions from code review
    Co-authored-by: sandcha <sandcha@users.noreply.github.com>

--- server/handlers/search_commune.py
+++ server/handlers/search_commune.py
@@ -6,8 +6,8 @@ class SearchCommune(object):
     def search(**params: dict) -> tuple:
         request_body = params["body"]
         if "extraitNomCommune" not in request_body:
-            return {"Error" : "Missing parameter extraitNomCommune"}, BAD_REQUEST
+            return {"Error" : "request missing parameter 'extraitNomCommune'"}, BAD_REQUEST
         extraitNomCommune = request_body["extraitNomCommune"]
         if extraitNomCommune == "":
-            return {"Error" : "parameter extraitNomCommune cannot be empty"}, BAD_REQUEST
+            return {"Error" : "request parameter 'extraitNomCommune' cannot be empty"}, BAD_REQUEST
         return search(extraitNomCommune), OK
