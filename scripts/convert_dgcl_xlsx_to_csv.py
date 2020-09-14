25435cda66d90ae67640b8fc99fbf1bb80c166ea
Augustin Wenger <magemax@yahoo.com>
2020-09-14 22:34:08 +0200

    style & typing

--- scripts/convert_dgcl_xlsx_to_csv.py
+++ scripts/convert_dgcl_xlsx_to_csv.py
@@ -1,8 +1,9 @@
-import pandas as pd
+import pandas as pd  # type: ignore
+
 
 def convert_col_names_2020_to_2019(data):
-    #Modifies column names that cchanged between 2019 and 2020
-    #We give them the name they had in 2019, cause our loader uses these.
+    # Modifies column names that cchanged between 2019 and 2020
+    # We give them the name they had in 2019, cause our loader uses these.
     dict_convert = {
         "Dotation de solidarité rurale - Cible - IS DSR Cible": "Dotation de solidarité rurale - Cible - Indice synthétique",
         "Dotation de solidarité rurale - Cible - Part Pfi": "Dotation de solidarité rurale - Cible - Part Pfi (avant garantie CN)",
@@ -17,11 +18,12 @@ def convert_col_names_2020_to_2019(data):
     data.columns = [dict_convert[k] if k in dict_convert else k for k in data.columns]
     return data
 
+
 def xlsxtocsv(filename):
-    df=pd.read_excel(filename,header=[0,1],dtype=str)  # Les noms de colonnes viennent sur 2 lignes dans le fichier DGCL.
-    col_name_separator=" - " 
+    df = pd.read_excel(filename, header=[0, 1], dtype=str)  # Les noms de colonnes viennent sur 2 lignes dans le fichier DGCL.
+    col_name_separator = " - "
     df.columns = df.columns.map(col_name_separator.join).str.strip(col_name_separator)  # On les concatène ainsi
-    nom_fichier=filename.split(".")[0]
-    df=df.replace(r'\.',",",regex=True)
-    df=convert_col_names_2020_to_2019(df)
-    df.to_csv(nom_fichier+".csv", index=False, decimal=",")
+    nom_fichier = filename.split(".")[0]
+    df = df.replace(r'\.', ",", regex=True)
+    df = convert_col_names_2020_to_2019(df)
+    df.to_csv(nom_fichier + ".csv", index=False, decimal=",")
