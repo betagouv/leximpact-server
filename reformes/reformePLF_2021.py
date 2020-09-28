dd3caefa3b45f64d2659e81688cb1e854f9bd3ce
Augustin Wenger <magemax@yahoo.com>
2020-09-28 16:07:36 +0200

    Includes reformes from PLF 2021

---
+++ reformes/reformePLF_2021.py
@@ -0,0 +1,24 @@
+
+reforme_PLF_2021 = {
+    "impot_revenu": {
+        "bareme": {
+            "seuils": [10084, 25710, 73516, 158122],
+            "taux": [0.11, 0.3, 0.41, 0.45],
+        },
+        "decote": {"seuil_celib": 779, "seuil_couple": 1289, "taux": 0.4525},
+        "plafond_qf": {
+            "abat_dom": {
+                "plaf_GuadMarReu": 2450,
+                "plaf_GuyMay": 4050,
+                "taux_GuadMarReu": 0.3,
+                "taux_GuyMay": 0.4,
+            },
+            "celib": 938,
+            "celib_enf": 3704,
+            "maries_ou_pacses": 1570,
+            "reduc_postplafond": 1565,
+            "reduc_postplafond_veuf": 1748
+        },
+    }
+}
+
