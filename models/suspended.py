4346d493ee0c9e45cc32a68b5db38fd76a5a18a5
benoit-cty <benoit.courty@assemblee-nationale.fr>
2021-03-04 22:19:46 +0100

    WIP : send mail when banned

--- models/suspended.py
+++ models/suspended.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, DateTime, Integer  # type: ignore
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+#from server.services import send_mail # TODO : make this import work !
 from typing import Any
+import logging
 import datetime
 
 Base: Any = declarative_base()
@@ -12,7 +14,6 @@ class Suspended(Base):
     email = Column(String())
     end_suspension = Column(DateTime())
 
-
 def create_suspension(session, email, delay_hours=1):
     suspended = Suspended(
         email=email,
@@ -20,8 +21,8 @@ def create_suspension(session, email, delay_hours=1):
     )
     session.add(suspended)
     session.commit()
+    alert_suspension(email, delay_hours)
     return True
-
 
 def count_active_suspensions(session, email):
     return (
@@ -31,3 +32,20 @@ def count_active_suspensions(session, email):
         )
         .count()
     )
+
+def alert_suspension(email, delay_hours):
+    email_team='benoit.courty@assemblee-nationale.fr'
+    content_team  = f"Bonjour\nL'utilisateur {email} vient d'être suspendu pour {delay_hours} heure(s).\nVotre dévoué serveur LexImpact"
+    print(content_team)
+    logging.error(f"BRUTE FORCE STOPPED : {content_team}")
+    send_mail(
+            recipient=email_team,
+            subject="Nouvelle suspension",
+            content=content_team,
+        )
+    send_mail(
+            recipient=email,
+            subject="Compte LexImpact suspendu",
+            content=f"Bonjour\nVotre compte LexImpact vient d'être suspendu pour {delay_hours} heure(s) en raison d'une utilisation trop importante.\nMerci de votre compréhension,\nL'équipe LexImpact",
+        )
+    return True
