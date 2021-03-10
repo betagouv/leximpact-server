b9484444ae927f5f7871ed61915c5c290ac32995
benoit-cty <benoit.courty@assemblee-nationale.fr>
2021-03-10 22:13:06 +0100

    WIP : test that we send a mail

--- server/services/auth_services.py
+++ server/services/auth_services.py
@@ -76,7 +76,7 @@ def login_user(session, email: str) -> Optional[JWT]:  # Optional[User]:
             return None
     return encode_jwt(JWT(), email)
 
-
+# Send an email when an account is suspended
 def alert_suspension(email, delay_hours, session):
     email_team = getenv("MAILJET_SENDER")
     content_team = f"Bonjour<br/>\n<p>L'utilisateur {email} vient d'être suspendu pour {delay_hours} heure(s).<br/> Il a fait plus de {limite_requetes} requêtes en {time_period_request.total_seconds()/-60} minutes.</p><br/>\nVotre dévoué serveur LexImpact"
--- tests/server/services/test_auth_services.py
+++ tests/server/services/test_auth_services.py
@@ -1,7 +1,11 @@
 from pytest import fixture  # type: ignore
 from models import User, create_user, find_user  # type: ignore
-from server.services import login_user  # type: ignore
+from server.services import login_user, check_user  # type: ignore
+from models import create_request, count_requests
+from datetime import datetime, timedelta
+#from models.request import create_request, count_requests
 
+from models import create_token, create_payload, create_nonce
 
 @fixture
 def email():
@@ -16,3 +20,30 @@ def user(session, email):
 
 def test_login_user_when_not_in_list(session):
     assert not login_user(session, "malevolent@villain.hacker")
+
+def test_too_much_request(session, user, email):
+    # Create token
+    jti = create_nonce()
+    iss, aud = "Moi", "Toi"
+    payload = create_payload(email, jti, iss, aud)
+    secret, algo = "asdf1234", "HS256"
+    tokenencoded = create_token(payload, secret, algo)
+    #tokenencoded = login_user(session, email)
+    # Do 19 requests
+    for i in range(0,20):
+        #create_request(session, email)
+        resultat = check_user(session, tokenencoded)
+        #print(resultat)
+        count = count_requests(session, email, since=timedelta(minutes=-2))
+        assert count == i + 1
+        assert resultat["success"] == True
+    count = count_requests(session, email, since=timedelta(minutes=-2))
+    assert count == 20
+    #print("Nombre de requêtes : ", count)
+    # One more call to ban the user
+    resultat = check_user(session, tokenencoded)
+    # Check banned
+    assert resultat["success"] == False
+    assert resultat["error"] == "User is suspended"
+    # Check mail is called
+    # TODO
