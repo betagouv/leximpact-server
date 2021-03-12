56948ecc82b11a34b0d6576751d126c09ee594fb
Benoit Courty <benoit.courty@assemblee-nationale.fr>
2021-03-12 20:21:50 +0100

    Mock call of send_mail

--- tests/server/services/test_auth_services.py
+++ tests/server/services/test_auth_services.py
@@ -1,10 +1,9 @@
 from pytest import fixture  # type: ignore
-from server.services import login_user, check_user  # type: ignore
-from models import create_user, find_user, count_requests
-from models import User, Request, Suspended  # type: ignore
+from server.services.auth_services import login_user, check_user  # type: ignore
+from models import create_user, find_user, count_requests, User, Request, Suspended, create_token, create_payload, create_nonce  # type: ignore
 from datetime import timedelta
 import mock
-from models import create_token, create_payload, create_nonce
+from unittest.mock import call
 
 
 @fixture
@@ -22,13 +21,15 @@ def test_login_user_when_not_in_list(session):
     assert not login_user(session, "malevolent@villain.hacker")
 
 
-def test_too_much_request(session, user, email):
+@fixture
+def clean_history(session):
     # Clean the call history
     session.query(Request).delete()
     session.query(Suspended).delete()
     session.commit()
-    my_count = count_requests(session, email, since=timedelta(minutes=-2))
-    # mocker.patch('server.services.mail_services.send_mail', return_value=True)
+
+
+def test_too_much_request(session, user, email, clean_history):
     # Create token
     jti = create_nonce()
     iss, aud = "Moi", "Toi"
@@ -37,36 +38,19 @@ def test_too_much_request(session, user, email):
     tokenencoded = create_token(payload, secret, algo)
     # Do 19 requests
     for i in range(0, 20):
-        # create_request(session, email)
         resultat = check_user(session, tokenencoded)
         assert resultat["success"] is True
-        # print(resultat)
         my_count = count_requests(session, email, since=timedelta(minutes=-2))
         assert my_count == i + 1
     my_count = count_requests(session, email, since=timedelta(minutes=-2))
     assert my_count == 20
-    # print("Nombre de requêtes : ", count)
     # One more call to ban the user
-    # 'mailjet_rest.Client'
-    # mail_services
-    # send_mail
-    # server.services.alert_suspension
-    # 'server.services.check_user'
-    # server.services.alert_suspension
-    print(__name__)
-    # with mock.patch.object(Client, 'create', return_value={"success" : False,
-    #         "error" : "Token has expired!"}) as mocker:
-    with mock.patch('server.services.send_mail', return_value=True) as mocker:
-
+    mock_path = 'server.services.auth_services.send_mail'
+    with mock.patch(mock_path) as mocker:
         resultat = check_user(session, tokenencoded)
-        # Check banned
-        assert resultat["success"] is False
-        assert resultat["error"] == "User is suspended"
-        # Check mail is called
-        print("called: " + str(mocker.called))
-        print("call_count: " + str(mocker.call_count))
-        print("call_args: " + str(mocker.call_args))
-        print("args_list: " + str(mocker.call_args_list))
-        print("mock calls:\n" + str(mocker.mock_calls))
-        print("method calls:\n " + str(mocker.method_calls))
-        # mock_sendmail.assert_called_with("any path")
+        # Check we send 2 mails
+        assert mocker.call_count == 2
+        # Tip : Just cut and past the result of mocker.call_args_list
+        expected = [call(content="Bonjour<br/>\n<p>L'utilisateur richard@leximpact.pop vient d'être suspendu pour 1 heure(s).<br/> Il a fait plus de 20 requêtes en 2.0 minutes.</p><br/>\nVotre dévoué serveur LexImpact", recipient='leximpact@an.fr', subject='Nouvelle suspension'), call(content="Bonjour<br/>\r\n<p>Votre compte LexImpact vient d'être suspendu pour 1 heure(s) en raison d'une utilisation trop importante.<br/>Ceci est requis pour la protection des données d’enquête vous permettant le calcul par déciles de population.</p>\nMerci de votre compréhension,<br/>\nL'équipe LexImpact", recipient='richard@leximpact.pop', subject='Compte LexImpact suspendu')]
+        # Check mail content
+        mocker.assert_has_calls(expected)
